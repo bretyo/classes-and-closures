5559725824b04ca5243da9a285787f474c078b1f
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-19 14:57:56 -0600

    Finished!

--- classes.js
+++ classes.js
@@ -30,6 +30,17 @@
 */
 
 //Code Here
+class Employee{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,7 +59,18 @@
 */
 
 //Code Here
-
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(employee){
+    this.reports.push(employee);
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,7 +94,36 @@
 */
 
 //Code Here
-
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age);
+    this.reports = [];
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  hire(employee){
+    super.hire();
+    this.reports.length === 0 ? (this.title = 'Not a manager') :
+    this.reports.length < 4 ? (this.title = 'Barely Manager') :
+    this.reports.length < 11 ? (this.title = 'Mostly Manager') :
+    this.reports.length < 51 ? (this.title = 'Manager') :
+    this.reports.length < 101 ? (this.title = 'Manager Plus') :
+    (this.title = 'Bestest Manager') ;
+  }
+  fire(index){
+    super.fire();
+    this.bonus += 100;
+    this.reports.length === 0 ? (this.title = 'Not a manager') :
+    this.reports.length < 4 ? (this.title = 'Barely Manager') :
+    this.reports.length < 11 ? (this.title = 'Mostly Manager') :
+    this.reports.length < 51 ? (this.title = 'Manager') :
+    this.reports.length < 101 ? (this.title = 'Manager Plus') :
+    (this.title = 'Bestest Manager') ;
+  }
+  updateTitle(){
+    
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +150,32 @@
 */
 
 //Code Here
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    let fifties = 0;
+    for (let i = this.widgets_made_count; i > 0; i -= 50) {
+      if(i > -1)fifties ++;
+      
+    }
+    this.wear_and_tear_count = fifties;
+  }
+
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+
+  reboot(){
+    return ()=>{
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    }
+  }
+}
 
 
